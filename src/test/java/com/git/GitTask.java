ddf7e14e9c4d223b41dc0b15da557f0ac0d22d4c
Sanjayvams <sanjayraj.selvajothi@gmail.com>
2022-11-09 22:46:37 +0530

    First

---
+++ src/test/java/com/git/GitTask.java
@@ -0,0 +1,15 @@
+package com.git;
+
+public class GitTask {
+	
+	public void test1() {
+		System.out.println("1");
+	}
+	public void test2() {
+		System.out.println("2");
+	}
+	public void test3() {
+		System.out.println("3");
+	}
+
+}
