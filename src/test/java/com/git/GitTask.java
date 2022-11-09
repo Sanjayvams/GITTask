e412a6f2acf2be06bde094bd647be5d89aa87ea6
Sanjayvams <sanjayraj.selvajothi@gmail.com>
2022-11-09 22:59:09 +0530

    NXT

--- src/test/java/com/git/GitTask.java
+++ src/test/java/com/git/GitTask.java
@@ -11,5 +11,13 @@ public class GitTask {
 	public void test3() {
 		System.out.println("3");
 	}
-
+	public void test4() {
+		System.out.println("4");
+	}
+	public void test5() {
+		System.out.println("5");
+	}
+	public void test6() {
+		System.out.println("6");
+	}
 }
